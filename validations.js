9b522484e30001c54b9c54595e561203778ac9ea
someoneacccount238 <someoneacccount238@gmail.com>
2024-11-25 10:40:23 +0200

    register changes

--- validations.js
+++ validations.js
@@ -2,12 +2,12 @@
 import { body } from 'express-validator';
 
 export const loginValidation = [
-  body('email', 'Неверный формат почты').isEmail(),
-  body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
+  // body('email', 'Неверный формат почты').isEmail(),
+  // body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
 ];
 
 export const registerValidation = [
-  body('email', 'Неверный формат почты').isEmail(),
-  body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 })
+  // body('email', 'Неверный формат почты').isEmail(),
+  // body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 })
 ];
  
